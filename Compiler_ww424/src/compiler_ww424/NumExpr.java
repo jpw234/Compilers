f84aed2d7b8ecc13a8d825d3a423d9082a22b807
Wen-Yu, Wang <ww424@cornell.edu>
2016-03-06 01:22:16 -0500

    add Char in numExpr

--- Compiler_ww424/src/compiler_ww424/NumExpr.java
+++ Compiler_ww424/src/compiler_ww424/NumExpr.java
@@ -1,18 +1,29 @@
 package compiler_ww424;
 
 public class NumExpr extends Expr {
-	private int value;
-	
-	public NumExpr(int i,int lineNum,int colNum) {
-		value = i;
-		line = lineNum;
-		column = colNum;
-	}
-	
-	public Type typecheck(SymTab s) {
-		return new Type("int");
-	}
-	public int getVal() {
-		return value;
-	}
+    private int intvalue;
+    private char charval;
+    private Boolean isChar = false;
+    
+    public NumExpr(int i,int lineNum,int colNum) {
+        intvalue = i;
+        line = lineNum;
+        column = colNum;
+    }
+    public NumExpr(char c, int lineNum, int colNum){
+        charval = c;
+        line = lineNum;
+        column = colNum;
+        isChar = true;
+    }
+    
+    public Type typecheck(SymTab s) {
+        return new Type("int");
+    }
+    public int getIntVal() {
+        return intvalue;
+    }
+    public char getCharVal(){
+        return charval;
+    }
 }
