88ef605da9680621cc03305a0e76443efefd88be
Jared Wright <jpw234@cornell.edu>
2016-03-06 01:07:17 -0500

    modified ReturnStmt to allow multiple returns

--- Compiler_ww424/src/compiler_ww424/ReturnStmt.java
+++ Compiler_ww424/src/compiler_ww424/ReturnStmt.java
@@ -1,22 +1,38 @@
 package compiler_ww424;
 
+import java.util.ArrayList;
+
 public class ReturnStmt extends Stmt{
-	private Expr val;
+	private ArrayList<Expr> val;
 	
-	public ReturnStmt(Expr v ,int lineNum,int colNum) {
+	public ReturnStmt(ArrayList<Expr> v ,int lineNum,int colNum) {
 		val = v;
 		line = lineNum;
 		column = colNum;
 	}
 	
-	public Expr getExpr() {
+	public ArrayList<Expr> getExpr() {
 		return val;
+	}
+	
+	public void addVal(Expr e) {
+		val.add(e);
 	}
 	
 	public Type typecheck(SymTab s) {
 		Type expected = s.getRetval();
 		if(expected == null) throw new Error("returned in scope not expecting return");
-		else if(!val.typecheck(s).equals(expected)) throw new Error("returned wrong type");
+		
+		Type given;
+		if(val.size() > 1) {
+			given = new Tuple(new ArrayList<Type>());
+			
+			for(int a = 0; a < val.size(); a++) {
+				given.add(val.get(a).typecheck(s));
+			}
+		}
+		else given = val.get(0).typecheck(s);
+		else if(!given.equals(expected)) throw new Error("returned wrong type");
 		else return val.typecheck(s);
 	}
 }
