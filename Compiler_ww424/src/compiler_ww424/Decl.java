d7e682f97508e16c934c376830c440689cb3edbe
Jared Wright <jpw234@cornell.edu>
2016-03-05 21:28:34 -0500

    updated Decl.java to include potential for defined array lengths

--- Compiler_ww424/src/compiler_ww424/Decl.java
+++ Compiler_ww424/src/compiler_ww424/Decl.java
@@ -1,12 +1,21 @@
 package compiler_ww424;
 
+import java.lang.reflect.Type;
+
 public class Decl extends Stmt {
 	private IDExpr name;
 	private Type type;
+	private List<Expr> accesses = null;
 	
 	public Decl(IDExpr n, Type t) {
 		name = n;
 		type = t;
+	}
+	
+	public Decl(IDExpr n, Type t, List<Expr> a) {
+		name = n;
+		type = t;
+		accesses = a;
 	}
 	
 	public IDExpr getName() {
@@ -17,10 +26,28 @@ public class Decl extends Stmt {
 		return type;
 	}
 	
+	public void addAccess(Expr a) {
+		accesses.add(a);
+	}
+	
+	public List<Expr> getAccesses() {
+		return accesses;
+	}
+	
 	public Type typecheck(SymTab s) {
 		if(s.lookup(name.getName()) != null) throw new Error("redeclared variable error");
 		
 		if(type.getType() != "underscore") s.add(name.getName(), type);
+		
+		Type dummyType = new Type("int");
+		
+		if(accesses != null) {
+			for(int a = 0; a < accesses.size(); a++) {
+				if(!dummyType.equals(accesses.get(a).typecheck(s))) {
+					throw new Error("non-integer expression used as array access");
+				}
+			}
+		}
 		
 		return new Type("unit");
 	}
