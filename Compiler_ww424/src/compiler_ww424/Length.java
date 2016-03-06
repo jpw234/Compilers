888fe5d8157528f3c5b62565973d6b36fa522b5a
Jared Wright <jpw234@cornell.edu>
2016-03-05 23:08:36 -0500

    added Length.java node

---
+++ Compiler_ww424/src/compiler_ww424/Length.java
@@ -0,0 +1,20 @@
+package compiler_ww424
+
+public class Length extends Expr {
+	private Expr arg;
+	
+	public Length(Expr e) {
+		arg = e;
+	}
+	
+	public Expr getArg() {
+		return arg;
+	}
+	
+	public Type typecheck(SymTab s) {
+		Type t = arg.typecheck(s);
+		if(t.getDepth() < 1) throw new Error("argument to Length() is not an array");
+		
+		return new Type("int");
+	}
+}
