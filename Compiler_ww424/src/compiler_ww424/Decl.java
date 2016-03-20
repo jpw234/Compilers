cc85e658274d9a7648750a5d817ad481532375fe
昱絜傅 <frostorb8000@dhcp-mcfad-1531.eduroam.cornell.edu>
2016-03-20 16:05:09 -0400

    fit IR simulator v2

--- Compiler_ww424/src/compiler_ww424/Decl.java
+++ Compiler_ww424/src/compiler_ww424/Decl.java
@@ -111,16 +111,16 @@ public class Decl extends Stmt {
 			IRTemp exprIdx = new IRTemp(_eI);
 			String _tmp = LabelMaker.Generate_Unique_Label("_tmp");
 			IRTemp tmp = new IRTemp(_tmp);// temp register
-			seqList.add(new IRMove(totalDim, new IRConst(accesses.size())));
-			seqList.add(new IRMove(exprIdx, 
+			seqList.add(new IRMove(new IRTemp(_tD), new IRConst(accesses.size())));
+			seqList.add(new IRMove(new IRTemp(_eI), 
 						new IRCall(
-								new IRName("_I_alloc_i"), new IRBinOp(IRBinOp.OpType.MUL, totalDim, new IRConst(8)))));
-			seqList.add(new IRMove(tmp, exprIdx));
+								new IRName("_I_alloc_i"), new IRBinOp(IRBinOp.OpType.MUL, new IRTemp(_tD), new IRConst(8)))));
+			seqList.add(new IRMove(new IRTemp(_tmp), new IRTemp(_eI)));
 			for(int i = 0; i < accesses.size(); i++){
-				seqList.add(new IRMove(new IRMem(exprIdx), accesses.get(i).buildIRExpr()));//exprIdx[i] = accesses[i]
-				seqList.add(new IRMove(exprIdx, new IRBinOp(IRBinOp.OpType.ADD, exprIdx, new IRConst(8))));//exprIdx += 8
+				seqList.add(new IRMove(new IRMem(new IRTemp(_eI)), accesses.get(i).buildIRExpr()));//exprIdx[i] = accesses[i]
+				seqList.add(new IRMove(new IRTemp(_eI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_eI), new IRConst(8))));//exprIdx += 8
 			}
-			seqList.add(new IRMove(exprIdx, tmp));//reset exprIdx pointer location
+			seqList.add(new IRMove(new IRTemp(_eI), new IRTemp(_tmp)));//reset exprIdx pointer location
 			//create registers
 			String _pDN = LabelMaker.Generate_Unique_Label("_preDimNum");
 			IRTemp preDimNum = new IRTemp(_pDN);//previous dimension number
@@ -145,80 +145,80 @@ public class Decl extends Stmt {
 			String _exitInit = LabelMaker.Generate_Unique_Label("_EXITINIT");//exit array initialization
 			String _skipLenLoc = LabelMaker.Generate_Unique_Label("_SLIPLENLOC");//skip length location
 			String _aP = LabelMaker.Generate_Unique_Label("_arrPointer");
-			IRTemp arrPointer = new IRTemp("_aP");
+			IRTemp arrPointer = new IRTemp(_aP);
 			String _pAP = LabelMaker.Generate_Unique_Label("_preArrPointer");
-			IRTemp preArrPointer = new IRTemp("_pAP");
+			IRTemp preArrPointer = new IRTemp(_pAP);
 			String _cU = LabelMaker.Generate_Unique_Label("_cycleUnit");//indicate each link operation cycle, cuz need to skip length memory location
-			IRTemp cycleUnit = new IRTemp("_cU");
+			IRTemp cycleUnit = new IRTemp(_cU);
 			//initial setting
-			seqList.add(new IRMove(preDimNum, new IRConst(1)));
-			seqList.add(new IRMove(curDimNum, new IRMem(exprIdx)));
-			seqList.add(new IRMove(recurFlag, new IRConst(0)));
-			seqList.add(new IRMove(dimIdx, new IRConst(0)));
+			seqList.add(new IRMove(new IRTemp(_pDN), new IRConst(1)));
+			seqList.add(new IRMove(new IRTemp(_cDN), new IRMem(new IRTemp(_eI))));
+			seqList.add(new IRMove(new IRTemp(_rF), new IRConst(0)));
+			seqList.add(new IRMove(new IRTemp(_dI), new IRConst(0)));
 			//allocate memory for each dimension
 			seqList.add(new IRLabel(_alloc));//label _alloc
-			seqList.add(new IRMove(tmp, 
-						new IRBinOp(IRBinOp.OpType.MUL, preDimNum, 
-								new IRBinOp(IRBinOp.OpType.ADD, curDimNum, new IRConst(1)))));//tmp = allocate size = prevDim*(curDimNum+1)
-			seqList.add(new IRMove(arrPointer, new IRCall(new IRName("_I_alloc_i"), new IRBinOp(IRBinOp.OpType.MUL, tmp, new IRConst(8)))));
-			seqList.add(new IRMove(new IRMem(arrPointer), curDimNum));//set length
-			seqList.add(new IRMove(loopIdx, new IRConst(1)));//set loopIdx = 1
-			seqList.add(new IRMove(tmp, new IRBinOp(IRBinOp.OpType.ADD, arrPointer, new IRConst(8))));//tmp = arrPointer + 8
-			seqList.add(new IRMove(dimIdx, new IRBinOp(IRBinOp.OpType.ADD, dimIdx, new IRConst(1))));//dimIdx++
+			seqList.add(new IRMove(new IRTemp(_tmp), 
+						new IRBinOp(IRBinOp.OpType.MUL, new IRTemp(_pDN), 
+								new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_cDN), new IRConst(1)))));//tmp = allocate size = prevDim*(curDimNum+1)
+			seqList.add(new IRMove(new IRTemp(_aP), new IRCall(new IRName("_I_alloc_i"), new IRBinOp(IRBinOp.OpType.MUL, new IRTemp(_tmp), new IRConst(8)))));
+			seqList.add(new IRMove(new IRMem(new IRTemp(_aP)), new IRTemp(_cDN)));//set length
+			seqList.add(new IRMove(new IRTemp(_lI), new IRConst(1)));//set loopIdx = 1
+			seqList.add(new IRMove(new IRTemp(_tmp), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_aP), new IRConst(8))));//tmp = arrPointer + 8
+			seqList.add(new IRMove(new IRTemp(_dI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_dI), new IRConst(1))));//dimIdx++
 			
 			//store starting position
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, recurFlag, new IRConst(1)), _length));//if recurFlag == 0, then record location
-			seqList.add(new IRMove(stLocation, new IRBinOp(IRBinOp.OpType.ADD, arrPointer, new IRConst(8))));//stLocation = arrPointer + 8
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, new IRTemp(_rF), new IRConst(1)), _length));//if recurFlag == 0, then record location
+			seqList.add(new IRMove(new IRTemp(_sL), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_aP), new IRConst(8))));//stLocation = arrPointer + 8
 			
 			//for loop for assign length
 			seqList.add(new IRLabel(_length));//label assign length
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, loopIdx, preDimNum), _checkRecursion));
-			seqList.add(new IRMove(arrPointer, 
-						new IRBinOp(IRBinOp.OpType.ADD, arrPointer, 
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, new IRTemp(_lI), new IRTemp(_pDN)), _checkRecursion));
+			seqList.add(new IRMove(new IRTemp(_aP), 
+						new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_aP), 
 						new IRBinOp(IRBinOp.OpType.MUL, new IRConst(8), 
-						new IRBinOp(IRBinOp.OpType.ADD, curDimNum, new IRConst(1))))));//arrPointer += 8*(curDimNum+1)
-			seqList.add(new IRMove(new IRMem(arrPointer), curDimNum));//set length
-			seqList.add(new IRMove(loopIdx, new IRBinOp(IRBinOp.OpType.ADD, loopIdx, new IRConst(1))));//loopIdx++
+						new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_cDN), new IRConst(1))))));//arrPointer += 8*(curDimNum+1)
+			seqList.add(new IRMove(new IRMem(new IRTemp(_aP)), new IRTemp(_cDN)));//set length
+			seqList.add(new IRMove(new IRTemp(_lI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_lI), new IRConst(1))));//loopIdx++
 			seqList.add(new IRJump(new IRName(_length)));
 			
 			seqList.add(new IRLabel(_checkRecursion));
-			seqList.add(new IRMove(arrPointer, tmp));//restore arrPointer original location
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, recurFlag, new IRConst(1)), _link));//if recurFlag==1 , jump to link operation
+			seqList.add(new IRMove(new IRTemp(_aP), new IRTemp(_tmp)));//restore arrPointer original location
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, new IRTemp(_rF), new IRConst(1)), _link));//if recurFlag==1 , jump to link operation
 			seqList.add(new IRJump(new IRName(_checkEnd)));//jump to checkEnd
 			
 			//for loop for link array pointer
 			seqList.add(new IRLabel(_link));
-			seqList.add(new IRMove(new IRMem(preArrPointer), arrPointer));//link Mem(preArrPointer) = arrPointer
-			seqList.add(new IRMove(loopIdx, new IRConst(1)));//set loopIdx = 1
-			seqList.add(new IRMove(subloopIdx, new IRConst(0)));//set subloopIdx = 0
+			seqList.add(new IRMove(new IRMem(new IRTemp(_pAP)), new IRTemp(_aP)));//link Mem(preArrPointer) = arrPointer
+			seqList.add(new IRMove(new IRTemp(_lI), new IRConst(1)));//set loopIdx = 1
+			seqList.add(new IRMove(new IRTemp(_slI), new IRConst(0)));//set subloopIdx = 0
 			seqList.add(new IRLabel(_linkLoop));
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, loopIdx, preDimNum), _checkEnd));//if loopIdx==preDimNum (finish linking), jump to checkEnd
-			seqList.add(new IRMove(preArrPointer, new IRBinOp(IRBinOp.OpType.ADD, preArrPointer, new IRConst(1))));//preArrPointer++
-			seqList.add(new IRMove(subloopIdx, new IRBinOp(IRBinOp.OpType.ADD, subloopIdx, new IRConst(1))));//subloopIdx++
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.NEQ, subloopIdx, cycleUnit), _skipLenLoc));//if subloopIdx != cycleUnit, jump to _skipLenLoc
-			seqList.add(new IRMove(preArrPointer, new IRBinOp(IRBinOp.OpType.ADD, preArrPointer, new IRConst(1))));//preArrPointer++
-			seqList.add(new IRMove(subloopIdx, new IRConst(0)));//set subloopIdx = 0
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, new IRTemp(_lI), new IRTemp(_pDN)), _checkEnd));//if loopIdx==preDimNum (finish linking), jump to checkEnd
+			seqList.add(new IRMove(new IRTemp(_pAP), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_pAP), new IRConst(1))));//preArrPointer++
+			seqList.add(new IRMove(new IRTemp(_slI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_slI), new IRConst(1))));//subloopIdx++
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.NEQ, new IRTemp(_slI), new IRTemp(_cU)), _skipLenLoc));//if subloopIdx != cycleUnit, jump to _skipLenLoc
+			seqList.add(new IRMove(new IRTemp(_pAP), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_pAP), new IRConst(1))));//preArrPointer++
+			seqList.add(new IRMove(new IRTemp(_slI), new IRConst(0)));//set subloopIdx = 0
 			seqList.add(new IRLabel(_skipLenLoc));//label _skipLenLoc
-			seqList.add(new IRMove(arrPointer, 
-						new IRBinOp(IRBinOp.OpType.ADD, arrPointer, 
-						new IRBinOp(IRBinOp.OpType.ADD, curDimNum, new IRConst(1)))));//arrPointer = arrPointer + (curDimNum + 1)
-			seqList.add(new IRMove(new IRMem(preArrPointer), arrPointer));//link Mem(preArrPointer) = arrPointer
-			seqList.add(new IRMove(loopIdx, new IRBinOp(IRBinOp.OpType.ADD, loopIdx, new IRConst(1))));//loopIdx++
+			seqList.add(new IRMove(new IRTemp(_aP), 
+						new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_aP), 
+						new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_cDN), new IRConst(1)))));//arrPointer = arrPointer + (curDimNum + 1)
+			seqList.add(new IRMove(new IRMem(new IRTemp(_pAP)), new IRTemp(_aP)));//link Mem(preArrPointer) = arrPointer
+			seqList.add(new IRMove(new IRTemp(_lI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_lI), new IRConst(1))));//loopIdx++
 			seqList.add(new IRJump(new IRName(_linkLoop)));
 			
 			seqList.add(new IRLabel(_checkEnd));//determine whether end the initialization
-			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, dimIdx, totalDim), _exitInit));
-			seqList.add(new IRMove(recurFlag, new IRConst(1)));//recurFlag = 1
-			seqList.add(new IRMove(cycleUnit, curDimNum));//cycleUnit = curDimNum
-			seqList.add(new IRMove(preDimNum, new IRBinOp(IRBinOp.OpType.MUL, preDimNum, curDimNum)));//preDimNum = preDimNum * curDimNum
-			seqList.add(new IRMove(exprIdx, new IRBinOp(IRBinOp.OpType.ADD, exprIdx, new IRConst(8))));//exprIdx += 8
-			seqList.add(new IRMove(curDimNum, new IRMem(exprIdx)));//curDimNum = Mem(exprIdx)
-			seqList.add(new IRMove(arrPointer, tmp));//arrPointer = tmp
-			seqList.add(new IRMove(preArrPointer, arrPointer));//preArrPointer = arrPointer
+			seqList.add(new IRCJump(new IRBinOp(IRBinOp.OpType.EQ, new IRTemp(_dI), new IRTemp(_tD)), _exitInit));
+			seqList.add(new IRMove(new IRTemp(_rF), new IRConst(1)));//recurFlag = 1
+			seqList.add(new IRMove(new IRTemp(_cU), new IRTemp(_cDN)));//cycleUnit = curDimNum
+			seqList.add(new IRMove(new IRTemp(_pDN), new IRBinOp(IRBinOp.OpType.MUL, new IRTemp(_pDN), new IRTemp(_cDN))));//preDimNum = preDimNum * curDimNum
+			seqList.add(new IRMove(new IRTemp(_eI), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(_eI), new IRConst(8))));//exprIdx += 8
+			seqList.add(new IRMove(new IRTemp(_cDN), new IRMem(new IRTemp(_eI))));//curDimNum = Mem(exprIdx)
+			seqList.add(new IRMove(new IRTemp(_aP), new IRTemp(_tmp)));//arrPointer = tmp
+			seqList.add(new IRMove(new IRTemp(_pAP), new IRTemp(_aP)));//preArrPointer = arrPointer
 			seqList.add(new IRJump(new IRName(_alloc)));
 			
 			seqList.add(new IRLabel(_exitInit));
-			seqList.add(new IRMove(new IRTemp(name.getName()), stLocation));//set variable to starting location
+			seqList.add(new IRMove(new IRTemp(name.getName()), new IRTemp(_sL)));//set variable to starting location
 			//delete exprIdx Mem
 			//exit
 			return new IRSeq(seqList);
