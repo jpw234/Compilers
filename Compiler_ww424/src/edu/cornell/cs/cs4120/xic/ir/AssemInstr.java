484c77925496048b4ce0cab52e28ae51b3d69c39
Jared Wright <jpw234@cornell.edu>
2016-04-10 21:47:05 -0500

    example AssemInstr.java

---
+++ Compiler_ww424/src/edu/cornell/cs/cs4120/xic/ir/AssemInstr.java
@@ -0,0 +1,28 @@
+package edu.cornell.cs.cs4120.xic.ir;
+
+public class AssemInstr {
+	String data = "";
+	String source = "";
+	
+	public AssemInstr(String dt, String src) {
+		data = dt;
+		source = src;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setData(String dt) {
+		data = dt;
+	}
+	
+	public void setSource(String src) {
+		source = src;
+	}
+
+}
