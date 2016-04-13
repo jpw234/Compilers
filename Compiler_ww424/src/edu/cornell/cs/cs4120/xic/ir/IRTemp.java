46205ce80732f7178f589d2080ee3dde18cd9780
Jared Wright <jpw234@cornell.edu>
2016-04-13 22:20:20 +0000

    correct assembly implementation for IRTemp

--- Compiler_ww424/src/edu/cornell/cs/cs4120/xic/ir/IRTemp.java
+++ Compiler_ww424/src/edu/cornell/cs/cs4120/xic/ir/IRTemp.java
@@ -54,12 +54,38 @@ public class IRTemp extends IRExpr {
     	else {
     		this.bestCost();
     		switch(bestTileNum) {
-    		case 0: {//mintile
-    			//TODO: implement correctly
-        		bestTile = new AssemInstr("", "", 0);
+    		case 0: {
+    			if(name.substring(0,4) == "_ARG") {
+    				int num = Integer.parseInt(name.substring(4));
+    				switch(num) {
+    				case 0: bestTile = new AssemInstr("", "%rdi", 0); break;
+    				case 1: bestTile = new AssemInstr("", "%rsi", 0); break;
+    				case 2: bestTile = new AssemInstr("", "%rdx", 0); break;
+    				case 3: bestTile = new AssemInstr("", "%rcx", 0); break;
+    				case 4: bestTile = new AssemInstr("", "%r8", 0); break;
+    				case 5: bestTile = new AssemInstr("", "%r9", 0); break;
+    				default: bestTile = new AssemInstr("", ((num-5)*8) + "(%rbp)", 0);
+    				}
+    			}
+    			else if(name.substring(0,4) == "_RET") {
+    				int num = Integer.parseInt(name.substring(4));
+    				switch(num) {
+    				case 0: bestTile = new AssemInstr("", "%rax", 0); break;
+    				case 1: bestTile = new AssemInstr("", "%rdx", 0); break;
+    				//TODO: THIS IS WRONG
+    				default: bestTile = new AssemInstr("", "%rax", 0);
+    				}
+    			}
+    			else if(name == "_CALLRET") bestTile = new AssemInstr("", "%rax", 0);
+    			else {
+    				bestTile = new AssemInstr("", StackAssigner.getLocation(name), 0);
+    			}
     		}
     		}
     	}
+    	//_ARG0-5, registers, 6+, ((n-5)*8)(%rbp)
+    	//_RET0 = %rax, _RET1 = %rdx, _RET2 = 0(%rdi), _RET3 = 8(%rdi), ...
+    	//_CALLRET = _RET0 = %rax
     	return bestTile;
     }
 }
