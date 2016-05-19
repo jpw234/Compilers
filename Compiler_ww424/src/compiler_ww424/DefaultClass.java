cb1c33ddddc8ac041e2894e1a971ba8a31e641f2
昱絜傅 <frostorb8000@dhcp-rhodes-676.eduroam.cornell.edu>
2016-05-19 07:22:44 -0400

    buildIR for defaultClass

--- Compiler_ww424/src/compiler_ww424/DefaultClass.java
+++ Compiler_ww424/src/compiler_ww424/DefaultClass.java
@@ -1,5 +1,7 @@
 package compiler_ww424;
 
+import java.util.List;
+import java.util.ArrayList;
 import edu.cornell.cs.cs4120.xic.ir.*;
 
 public class DefaultClass extends Expr {
@@ -22,7 +24,16 @@ public class DefaultClass extends Expr {
 	
 	//TODO: implement buildIRExpr()
 	public IRExpr buildIRExpr() {
-		return null;
+		String _this = LabelMaker.Generate_Unique_Label("_this");
+		String _temp = LabelMaker.Generate_Unique_Label("_temp");
+		String sz = "_I_size_" + className;
+		String vt = "_I_vt_" + className;
+		List<IRStmt> seqList = new ArrayList<IRStmt>();
+		seqList.add(new IRMove(new IRTemp(_this), new IRCall(new IRName("_I_alloc_i"), 
+				new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(sz), new IRTemp("_reg_%rip")))));//allocate fields space & return "this" value
+		seqList.add(new IRMove(new IRTemp(_temp), new IRBinOp(IRBinOp.OpType.ADD, new IRTemp(vt), new IRTemp("_reg_%rip"))));//compute dispatch vector address
+		seqList.add(new IRMove(new IRMem(new IRTemp(_this)), new IRTemp(_temp)));
+		return new IRESeq(new IRSeq(seqList), new IRTemp(_this));
 	}
 	@Override
 	public String toString(){
